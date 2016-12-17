43cece1eb1cf4abb238740fc9b45112e31544c0d
Kelly Selden <kellyselden@gmail.com>
2016-12-17 00:15:19 -0800

    fix for literal keys

--- addon/computed.js
+++ addon/computed.js
@@ -1,6 +1,8 @@
 import buildComputed from './-build-computed';
+import collapseKeys from './collapse-keys';
 import getValue from './get-value';
+import flattenKeys from './flatten-keys';
 
 export default function(...args) {
-  return buildComputed(args, getValue);
+  return buildComputed(args, collapseKeys, getValue, flattenKeys);
 }
