64c0362e781d27d00760ec496879e6034f3c60ee
Kelly Selden <kellyselden@gmail.com>
2017-01-18 08:50:37 -0800

    changes needed to support ember-cpm

---
+++ tests/unit/normalize-array-key-test.js
@@ -0,0 +1,18 @@
+import normalizeArrayKey from 'ember-macro-helpers/normalize-array-key';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | normalize array key');
+
+test('it wraps array if string', function(assert) {
+  let result = normalizeArrayKey('array');
+
+  assert.strictEqual(result, 'array.[]');
+});
+
+test('it does nothing for non-string', function(assert) {
+  let obj = {};
+
+  let result = normalizeArrayKey(obj);
+
+  assert.strictEqual(result, obj);
+});
