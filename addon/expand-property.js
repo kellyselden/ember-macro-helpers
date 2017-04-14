35b5a20e478df7c54022ab9fc954a2e0ff8e943b
Kelly Selden <kellyselden@gmail.com>
2017-04-14 12:27:36 -0700

    expand and collapse refactor to have better separation of concerns breaking change for anyone using these directly, but not for the consumers like `computed`

--- addon/expand-property.js
+++ addon/expand-property.js
@@ -3,13 +3,6 @@ import Ember from 'ember';
 const { expandProperties } = Ember;
 
 export default function(property) {
-  let atEachIndex = property.indexOf('.@each');
-  if (atEachIndex !== -1) {
-    return [property.slice(0, atEachIndex)];
-  } else if (property.slice(-2) === '[]') {
-    return [property.slice(0, -3)];
-  }
-
   let newPropertyList = [];
   expandProperties(property, expandedProperties => {
     newPropertyList = newPropertyList.concat(expandedProperties);
