788d8cf0c8665071dd9ddfa8a3cc768f354cbf23
Kelly Selden <kellyselden@gmail.com>
2017-04-14 21:36:13 -0700

    allow property merging in normalize-array-key

--- addon/normalize-array-key.js
+++ addon/normalize-array-key.js
@@ -1,6 +1,45 @@
-export default function(key) {
-  if (typeof key === 'string') {
-    key += '.[]';
+export default function(array, keys = []) {
+  if (typeof array !== 'string') {
+    return array;
   }
-  return key;
+
+  let props;
+
+  let i = array.indexOf('.@each');
+  if (i !== -1) {
+    let chain = array.split('.');
+    let end = chain[chain.length - 1];
+    if (end.indexOf('{') === 0) {
+      props = end.substring(1, end.length - 1).split(',');
+    } else {
+      props = [end];
+    }
+  } else {
+    i = array.indexOf('.[]');
+    props = [];
+  }
+
+  if (i !== -1) {
+    array = array.substr(0, i);
+  }
+
+  keys.forEach(key => {
+    if (props.indexOf(key) === -1) {
+      props.push(key);
+    }
+  });
+
+  let suffix;
+  if (props.length === 0) {
+    suffix = '[]';
+  } else {
+    suffix = '@each.';
+    if (props.length === 1) {
+      suffix += props[0];
+    } else {
+      suffix += `{${props.join(',')}}`;
+    }
+  }
+
+  return `${array}.${suffix}`;
 }
--- tests/unit/normalize-array-key-test.js
+++ tests/unit/normalize-array-key-test.js
@@ -4,9 +4,15 @@ import { module, test } from 'qunit';
 module('Unit | normalize array key');
 
 test('it wraps array if string', function(assert) {
-  let result = normalizeArrayKey('array');
+  let result = normalizeArrayKey('foo.bar');
 
-  assert.strictEqual(result, 'array.[]');
+  assert.strictEqual(result, 'foo.bar.[]');
+});
+
+test('it wraps array if string and empty keys', function(assert) {
+  let result = normalizeArrayKey('foo.bar', []);
+
+  assert.strictEqual(result, 'foo.bar.[]');
 });
 
 test('it does nothing for non-string', function(assert) {
@@ -15,4 +21,74 @@ test('it does nothing for non-string', function(assert) {
   let result = normalizeArrayKey(obj);
 
   assert.strictEqual(result, obj);
+});
+
+test('it does nothing for non-string and empty keys', function(assert) {
+  let obj = {};
+
+  let result = normalizeArrayKey(obj, []);
+
+  assert.strictEqual(result, obj);
+});
+
+test('it does nothing for non-string and keys', function(assert) {
+  let obj = {};
+
+  let result = normalizeArrayKey(obj, ['prop']);
+
+  assert.strictEqual(result, obj);
+});
+
+test('it does not alter array.[]', function(assert) {
+  let result = normalizeArrayKey('foo.bar.[]');
+
+  assert.strictEqual(result, 'foo.bar.[]');
+});
+
+test('it does not alter array.[] with empty keys', function(assert) {
+  let result = normalizeArrayKey('foo.bar.[]', []);
+
+  assert.strictEqual(result, 'foo.bar.[]');
+});
+
+test('it does not alter array.@each.prop', function(assert) {
+  let result = normalizeArrayKey('foo.bar.@each.prop');
+
+  assert.strictEqual(result, 'foo.bar.@each.prop');
+});
+
+test('it does not alter array.@each.prop with empty keys', function(assert) {
+  let result = normalizeArrayKey('foo.bar.@each.prop', []);
+
+  assert.strictEqual(result, 'foo.bar.@each.prop');
+});
+
+test('it watches array prop when raw array', function(assert) {
+  let result = normalizeArrayKey('foo.bar', ['prop']);
+
+  assert.strictEqual(result, 'foo.bar.@each.prop');
+});
+
+test('it watches array prop when already watching array length', function(assert) {
+  let result = normalizeArrayKey('foo.bar.[]', ['prop']);
+
+  assert.strictEqual(result, 'foo.bar.@each.prop');
+});
+
+test('it watches multiple array props when already watching array length', function(assert) {
+  let result = normalizeArrayKey('foo.bar.[]', ['prop', 'prop2']);
+
+  assert.strictEqual(result, 'foo.bar.@each.{prop,prop2}');
+});
+
+test('it watches array prop when already watching array prop', function(assert) {
+  let result = normalizeArrayKey('foo.bar.@each.baz', ['prop']);
+
+  assert.strictEqual(result, 'foo.bar.@each.{baz,prop}');
+});
+
+test('it watches multiple array props when already watching multiple array props', function(assert) {
+  let result = normalizeArrayKey('foo.bar.@each.{baz,baz2}', ['prop', 'prop2']);
+
+  assert.strictEqual(result, 'foo.bar.@each.{baz,baz2,prop,prop2}');
 });
