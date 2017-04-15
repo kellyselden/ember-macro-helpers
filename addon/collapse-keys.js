71f1367550b5e06e91eef78cce009260e48abaeb
Kelly Selden <kellyselden@gmail.com>
2017-04-14 21:44:17 -0700

    add keyMap to collapseKeys

--- addon/collapse-keys.js
+++ addon/collapse-keys.js
@@ -2,15 +2,25 @@ import collapseKey from './collapse-key';
 
 export function collapseKeysWithMap(keys) {
   let collapsedKeys = [];
+  let keyMap = [];
 
   keys.forEach(key => {
     let array = collapseKey(key);
 
     collapsedKeys = collapsedKeys.concat(array);
+
+    let i;
+    if (keyMap.length) {
+      i = keyMap[keyMap.length - 1] + 1;
+    } else {
+      i = 0;
+    }
+    keyMap = keyMap.concat(array.map(() => i));
   });
 
   return {
-    collapsedKeys
+    collapsedKeys,
+    keyMap
   }
 }
 
