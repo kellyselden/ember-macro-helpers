df11df138774b28de4d4216a2f3f5eaf69f5a326
Kelly Selden <kellyselden@gmail.com>
2017-04-14 21:44:17 -0700

    add keyMap to collapseKeys

--- tests/unit/collapse-keys-test.js
+++ tests/unit/collapse-keys-test.js
@@ -1,4 +1,4 @@
-import collapseKeys from 'ember-macro-helpers/collapse-keys';
+import collapseKeys, { collapseKeysWithMap } from 'ember-macro-helpers/collapse-keys';
 import { module, test } from 'qunit';
 
 module('Unit | collapse keys');
@@ -49,5 +49,54 @@ test('it collapses brace expansion', function(assert) {
   assert.deepEqual(
     collapseKeys(['foo.{bar,baz}', 'one.{two,three}']),
     ['foo.bar', 'foo.baz', 'one.two', 'one.three']
+  );
+});
+
+test('keyMap: it allows empty arrays', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap([]).keyMap,
+    []
+  );
+});
+
+test('keyMap: it ignores non-strings', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap([{}]).keyMap,
+    [0]
+  );
+});
+
+test('keyMap: it ignores array.[]', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap(['foo.[]']).keyMap,
+    [0]
+  );
+});
+
+test('keyMap: it ignores array.@each', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap(['foo.@each.bar']).keyMap,
+    [0]
+  );
+});
+
+test('keyMap: it ignores array.@each with brace expansion', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap(['foo.@each.{bar,baz}']).keyMap,
+    [0]
+  );
+});
+
+test('keyMap: it ignores string without brace expansion', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap(['foo', 'one']).keyMap,
+    [0, 1]
+  );
+});
+
+test('keyMap: it collapses brace expansion', function(assert) {
+  assert.deepEqual(
+    collapseKeysWithMap(['foo.{bar,baz}', 'one.{two,three}']).keyMap,
+    [0, 0, 1, 1]
   );
 });
