e5768a2e6bbc6324859d7558cdb464d94b562198
Kelly Selden <kellyselden@gmail.com>
2017-04-24 21:47:10 -0700

    use optional params system for buildComputed

--- addon/computed.js
+++ addon/computed.js
@@ -4,5 +4,5 @@ import getValue from './get-value';
 import flattenKeys from './flatten-keys';
 
 export default function(...args) {
-  return buildComputed(args, collapseKeys, getValue, flattenKeys);
+  return buildComputed({ args, collapseKeys, getValue, flattenKeys });
 }
