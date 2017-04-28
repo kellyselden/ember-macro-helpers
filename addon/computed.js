645925113b556ce4103a10baac77af80893be522
Kelly Selden <kellyselden@gmail.com>
2017-04-27 20:45:18 -0700

    use currying in build-computed

--- addon/computed.js
+++ addon/computed.js
@@ -3,6 +3,4 @@ import collapseKeys from './collapse-keys';
 import getValue from './get-value';
 import flattenKeys from './flatten-keys';
 
-export default function(...args) {
-  return buildComputed({ args, collapseKeys, getValue, flattenKeys });
-}
+export default buildComputed({ collapseKeys, getValue, flattenKeys });
