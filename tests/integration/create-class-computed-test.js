5ea1c692270eba01bb0eb0e8044db4f718b0110f
Kelly Selden <kellyselden@gmail.com>
2017-05-17 23:25:14 -0700

    add class computed composing tests

--- tests/integration/create-class-computed-test.js
+++ tests/integration/create-class-computed-test.js
@@ -24,7 +24,7 @@ module('Integration | create class computed', {
         }
         return computed(normalizeArrayKey(array, [key]), value, function(array, value) {
           if (array) {
-            return array.filterBy(key || PROPERTIES.get(this), value);
+            return emberA(emberA(array).filterBy(key || PROPERTIES.get(this), value));
           }
         });
       }
@@ -89,7 +89,7 @@ test('it responds to array property value changes externally', function(assert) 
   assert.equal(subject.get('computed.length'), 2);
 });
 
-test('it responds to array property value changes using composing', function(assert) {
+test('it responds to array property value changes using raw array', function(assert) {
   let array = emberA([
     EmberObject.create({ test: 'val1' }),
     EmberObject.create({ test: 'val2' })
@@ -184,6 +184,145 @@ test('it responds to property value changes', function(assert) {
   subject.set('value', 'val1');
 
   assert.equal(subject.get('computed.length'), 2);
+});
+
+test('composing: macros still compute inside', function(assert) {
+  let array1 = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val1' })
+  ]);
+  let array2 = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val2' })
+  ]);
+
+  let { subject } = compute({
+    computed: filterBy(computed('innerKey', innerKey => innerKey ? array1 : array2), 'key', 'value'),
+    properties: {
+      innerKey: true,
+      key: 'test',
+      value: 'val1'
+    }
+  });
+
+  assert.equal(subject.get('computed.length'), 2);
+
+  subject.set('innerKey', false);
+
+  assert.equal(subject.get('computed.length'), 1);
+});
+
+test('composing: it responds to array property value changes', function(assert) {
+  let array = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val2' })
+  ]);
+
+  let { subject } = compute({
+    computed: filterBy(computed(() => array), 'key', 'value'),
+    properties: {
+      key: 'test',
+      value: 'val1'
+    }
+  });
+
+  assert.equal(subject.get('computed.length'), 1);
+
+  array.set('1.test', 'val1');
+
+  assert.equal(subject.get('computed.length'), 2);
+});
+
+test('composing: it responds to property key changes', function(assert) {
+  let array = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val2' })
+  ]);
+
+  let { subject } = compute({
+    computed: filterBy(computed(() => array), 'key', 'value'),
+    properties: {
+      array,
+      key: 'test2',
+      value: 'val1'
+    }
+  });
+
+  assert.equal(subject.get('computed.length'), 0);
+
+  subject.set('key', 'test');
+
+  assert.equal(subject.get('computed.length'), 1);
+});
+
+test('composing: it responds to property value changes', function(assert) {
+  let array = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val1' })
+  ]);
+
+  let { subject } = compute({
+    computed: filterBy(computed(() => array), 'key', 'value'),
+    properties: {
+      array,
+      key: 'test',
+      value: 'val2'
+    }
+  });
+
+  assert.equal(subject.get('computed.length'), 0);
+
+  subject.set('value', 'val1');
+
+  assert.equal(subject.get('computed.length'), 2);
+});
+
+test('composing: macros still compute outside', function(assert) {
+  let array1 = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val1' })
+  ]);
+  let array2 = emberA([
+    EmberObject.create({ test: 'val1' }),
+    EmberObject.create({ test: 'val2' })
+  ]);
+
+  let { subject } = compute({
+    computed: computed(filterBy('array', 'key', 'value'), array => array),
+    properties: {
+      array: array1,
+      key: 'test',
+      value: 'val1'
+    }
+  });
+
+  assert.equal(subject.get('computed.length'), 2);
+
+  subject.set('array', array2);
+
+  assert.equal(subject.get('computed.length'), 1);
+});
+
+test('composing: both macros are class computed', function(assert) {
+  let array = emberA([
+    EmberObject.create({ id: 1, test1: 'val1', test2: 'val1' }),
+    EmberObject.create({ id: 2, test1: 'val2', test2: 'val1' }),
+    EmberObject.create({ id: 3, test1: 'val2', test2: 'val2' }),
+    EmberObject.create({ id: 3, test1: 'val1', test2: 'val2' })
+  ]);
+
+  let { subject } = compute({
+    computed: filterBy(filterBy('array', 'key1', 'value1'), 'key2', 'value2'),
+    properties: {
+      array,
+      key1: 'test1',
+      key2: 'test2',
+      value1: 'val1',
+      value2: 'val1'
+    }
+  });
+
+  assert.deepEqual(subject.get('computed').mapBy('id'), [1]);
 });
 
 test('it cleans up after destroy', function(assert) {
