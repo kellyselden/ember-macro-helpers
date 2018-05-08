ef5c9e07712fdaf6f109566ca8975f50d53ed2d8
Kelly Selden <kellyselden@gmail.com>
2018-05-08 16:29:18 -0700

    use new test syntax in blueprint (#198)

--- blueprints/macro-test/files/tests/unit/macros/__name__-test.js
+++ blueprints/macro-test/files/tests/unit/macros/__name__-test.js
@@ -1,18 +1,19 @@
 import <%= camelizedModuleName %> from '<%= dasherizedModulePrefix %>/macros/<%= dasherizedModuleName %>';
-import compute from 'ember-macro-test-helpers/compute';
 import { module, test } from 'qunit';
+import compute from 'ember-macro-test-helpers/compute';
 
-module('<%= friendlyTestName %>');
+module('<%= friendlyTestName %>', function(hooks) {
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  compute({
-    assert,
-    computed: <%= camelizedModuleName %>('key1', 'key2'),
-    properties: {
-      key1: 1,
-      key2: 2
-    },
-    strictEqual: 3
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    compute({
+      assert,
+      computed: <%= camelizedModuleName %>('key1', 'key2'),
+      properties: {
+        key1: 1,
+        key2: 2
+      },
+      strictEqual: 3
+    });
   });
 });
