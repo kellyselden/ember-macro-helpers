7727e85b674b2d20be1e9e61d147b7c9acc5fe73
Kelly Selden <kellyselden@gmail.com>
2018-08-06 10:01:30 +0100

    ember-cli-update

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -1,4 +1,4 @@
-import { click, find, visit } from '@ember/test-helpers';
+import { click, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 
@@ -8,28 +8,28 @@ module('Acceptance | application', function(hooks) {
   test('double render failing test', async function(assert) {
     await visit('/double-render');
 
-    assert.equal(find('.computed').textContent, 'test val 1');
+    assert.dom('.computed').hasText('test val 1');
 
     await click('button');
 
-    assert.equal(find('.computed').textContent, 'test val 2');
+    assert.dom('.computed').hasText('test val 2');
 
     await click('button');
 
-    assert.equal(find('.computed').textContent, 'test val 3');
+    assert.dom('.computed').hasText('test val 3');
   });
 
   test('no rerender failing test', async function(assert) {
     await visit('/no-rerender');
 
-    assert.equal(find('.items').textContent, 1);
+    assert.dom('.items').hasText('1');
 
     await click('button');
 
-    assert.equal(find('.items').textContent, 0);
+    assert.dom('.items').hasText('0');
 
     await click('button');
 
-    assert.equal(find('.items').textContent, 1);
+    assert.dom('.items').hasText('1');
   });
 });
