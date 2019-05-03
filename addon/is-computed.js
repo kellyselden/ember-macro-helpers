6bf4ce7cf5f2364d5d94466b95584a4695a54015
Kelly Selden <kellyselden@gmail.com>
2019-05-03 11:22:38 +0100

    fix linting

--- addon/is-computed.js
+++ addon/is-computed.js
@@ -1,8 +1,6 @@
 import ComputedProperty from '@ember/object/computed';
 
 export default function(key) {
-  return (
-    key instanceof ComputedProperty ||
-    (typeof key === 'function' && '_getter' in key)
-  );
+  return key instanceof ComputedProperty ||
+    typeof key === 'function' && '_getter' in key;
 }
