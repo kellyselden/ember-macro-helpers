0f6a700ceaf11b603dacaa9e79cb7cab189be7d9
Kelly Selden <kellyselden@gmail.com>
2019-07-19 09:15:43 +0100

    fix vendor

--- vendor/monkey-patch-ember-computed.js
+++ vendor/monkey-patch-ember-computed.js
@@ -1,32 +1,40 @@
 (function() {
-  let computedStore = new WeakMap();
+  var computedStore = new WeakMap();
 
-  define('ember-macro-helpers/-computed-store', () => ({
-    'default': computedStore,
-    __esModule: true,
-  }));
+  define('ember-macro-helpers/-computed-store', function() {
+    return {
+      'default': computedStore,
+      __esModule: true
+    };
+  });
 
-  let _computed = Ember.computed;
+  var _computed = Ember.computed;
 
   // attempt to monkey patch before anything else runs
-  Ember.computed = function computed(...args) {
-    let cp = _computed.apply(this, args);
-    let dependentKeys = args.slice(0, args.length - 1);
-    let getter = args[args.length - 1];
-    computedStore.set(cp, { dependentKeys, getter });
+  Ember.computed = function computed() {
+    var args = Array.prototype.slice.call(arguments);
+    var cp = _computed.apply(this, args);
+    var dependentKeys = args.slice(0, args.length - 1);
+    var getter = args[args.length - 1];
+    computedStore.set(cp, {
+      dependentKeys: dependentKeys,
+      getter: getter
+    });
     return cp;
   };
 
   // support built-in macros
-  for (let _macro in _computed) {
-    if (_computed.hasOwnProperty(_macro)) {
-      Ember.computed[_macro] = function macro(...args) {
-        let cp = _computed[_macro].apply(this, args);
-        let dependentKeys = args;
-        let getter = cp._getter; // unfortunate private access (is there a better way?)
-        computedStore.set(cp, { dependentKeys, getter });
-        return cp;
-      };
-    }
-  }
+  Object.keys(_computed).forEach(function(_macro) {
+    Ember.computed[_macro] = function macro() {
+      var args = Array.prototype.slice.call(arguments);
+      var cp = _computed[_macro].apply(this, args);
+      var dependentKeys = args;
+      var getter = cp._getter; // unfortunate private access (is there a better way?)
+      computedStore.set(cp, {
+        dependentKeys: dependentKeys,
+        getter: getter
+      });
+      return cp;
+    };
+  });
 })();
