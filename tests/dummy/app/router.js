fd670204b56efb2a59959feeb0e9209a37873af0
Kelly Selden <kellyselden@gmail.com>
2019-11-21 15:58:14 +0000

    ember-cli 3.14

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,14 +1,12 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('double-render');
   this.route('no-rerender');
 });
-
-export default Router;
